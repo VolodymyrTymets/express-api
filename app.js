600323c71d47556f51bebe363959aa143930e1e2
volodymyr <volodymyrtymets@gmail.com>
2018-09-14 14:43:07 +0300

    add first swager doc

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+
 require('dotenv').config();
+
+
+const SwaggerExpress = require('swagger-express-mw');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 const bodyParser = require('body-parser');
 const { config } = require('./config');
@@ -10,6 +16,8 @@ const api = require('./src/api/index');
 const { passport } = require('./src/passport');
 const { mongoManager } = require('./src/mongo');
 const { onAppStart } = require('./on-start');
+
+const swaggerDocument = YAML.load('./swagger/swagger.yaml');
 
 const app = express();
 mongoManager.connect();
@@ -32,6 +40,17 @@ app.use(passport.init());
 
 // api routes v1
 app.use('/api/v1', api(config));
+
+
+// register api doc
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+SwaggerExpress.create({
+	appRoot: __dirname,
+	swaggerFile:path.resolve(__dirname, './swagger/swagger.yaml'),
+}, (err, swaggerExpress) => {
+	if (err) { throw err; }
+	swaggerExpress.register(app);
+});
 
 // on App start
 //onAppStart();
