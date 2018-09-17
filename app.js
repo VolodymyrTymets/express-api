b33574baebc823837b9f58aa2ac66aae857d268e
volodymyr <volodymyrtymets@gmail.com>
2018-09-17 10:35:29 +0300

    add restart server for doc v.0.2

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const fs = require('fs');
 const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
-
 const SwaggerExpress = require('swagger-express-mw');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const swaggerMerger = require('swagger-merger')
 
 const bodyParser = require('body-parser');
 const { config } = require('./config');
@@ -17,7 +18,7 @@ const { passport } = require('./src/passport');
 const { mongoManager } = require('./src/mongo');
 const { onAppStart } = require('./on-start');
 
-const swaggerDocument = YAML.load('./swagger/swagger.yaml');
+
 
 const app = express();
 mongoManager.connect();
@@ -43,16 +44,18 @@ app.use('/api/v1', api(config));
 
 
 // register api doc
+const outputSwaggerDir = path.resolve(config.swaggerDirPath, './build');
+const swaggerBuildFilePath = path.resolve(outputSwaggerDir, './swagger.yaml');
+
+if(!fs.existsSync(outputSwaggerDir)) {
+	fs.mkdirSync(outputSwaggerDir);
+}
+swaggerMerger({ input: config.swaggerFilePath, output: swaggerBuildFilePath });
+const swaggerDocument = YAML.load(swaggerBuildFilePath);
 app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-SwaggerExpress.create({
-	appRoot: __dirname,
-	swaggerFile:path.resolve(__dirname, './swagger/swagger.yaml'),
-}, (err, swaggerExpress) => {
-	if (err) { throw err; }
-	swaggerExpress.register(app);
-});
 
 // on App start
 //onAppStart();
+
 
 module.exports = app;
