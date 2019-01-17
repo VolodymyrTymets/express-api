1d18895b61c5f824e6858229b7746253c2a88f54
volodymyr <volodymyrtymets@gmail.com>
2019-01-17 11:50:17 +0200

    add missed code

--- src/models/car/model.js
+++ src/models/car/model.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const { schema } = require('./schema');
-const Car = mongoose.model('Car', schema);
+const { fieldToSearch } = require('../../utils/mongo');
 
+schema.methods.fieldsToSearch = search => [
+  'model'
+].map(fieldToSearch(search));
+
+const Car = mongoose.model('Car', schema);
 module.exports = { Car };
